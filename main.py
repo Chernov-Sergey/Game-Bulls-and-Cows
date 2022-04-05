3fb93f5b0b07675dd18bb0f7b613f5ef32533294
Sergey Chernov <msg2box@gmail.com>
2022-04-05 21:11:33 +0300

    Initial commit,ver 0.2

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from mastermind_engine import enter_number, check_number
+from mastermind_engine import enter_number, check_number, ai_step
 from termcolor import cprint, colored
 
-num = int(input(""))
+ai_step()
+
+while True:
+    user_step = str(input('Ваш ход'))
+    enter_number(user_step)
+    step = check_number()
+    if step('win') == 'True':
+        print('Yoy win')
+        print('Steps=', step['step'])
+        break
+    else:
+        print('Bulls=', step['bulls'], ', Cows=', step['cows'])
--- mastermind_engine.py
+++ mastermind_engine.py
@@ -1,27 +1,49 @@
 from random import randint
 
+global step_count
+step_count = 0
+
+
+
 def ai_step():
     global res
     res = []
 
-
-
     while True:
-        result = randint(1000, 9999)
-        res = len(str(result))
-        for i in range(0, len(res + 1)):
-            for j in range(1, len(res +1)):
-                if res[i] == res[j]:
-                    break
-
+        random_num = randint(1000, 9999)
+        res = list(str(random_num))
+        res_set = set(res)
+        if len(res_set) == len(res):
+            break
 
 
 
 def enter_number(user_number):
-    pass
+    global user_num
+    user_num = user_number
+    user_num = list(str(user_num))
 
 
 def check_number():
-    pass
+    global step_count
+    step_count = step_count + 1
+    bulls, cows = 0, 0
+    for i in range(len(user_num)):
+        for j in range(len(res)):
+            if user_num[i] == res[j]:
+                bulls += 1
+    user_num_set = set(user_num)
+    res_set = set(res)
+    total_set = user_num_set.union(res_set)
+    cows = 8 - len(total_set) - bulls
+    answer = {'bulls': bulls, 'cows': cows, 'step': step_count, 'win': 'False'}
+    if bulls == 4:
+        answer['win'] = 'True'
+    return answer
+
+# def is_gameover():
+#     return
+
+
 
 
